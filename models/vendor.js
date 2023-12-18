ff7b1a6b70b48f4c453441e2a7bc2c65753fc5de
Alyssa Alonso <aalonso310@gmail.com>
2023-12-18 11:36:22 -0500

    create vendor model

---
+++ models/vendor.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const vendorSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    markets: {
+      type: Schema.Types.ObjectId,
+      ref: "Market",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Vendor", vendorSchema);
