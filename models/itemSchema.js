f332e66b1bb835b226a91bfcb1b01f9c5eea5bc0
Alyssa Alonso <aalonso310@gmail.com>
2023-12-18 11:01:05 -0500

    update itemSchema and create reviewSchema

--- models/itemSchema.js
+++ models/itemSchema.js
@@ -1,12 +1,40 @@
-const Schema = require('mongoose').Schema;
+const Schema = require("mongoose").Schema;
 
-const itemSchema = new Schema({
-  name: { type: String, required: true },
-  emoji: String,
-  category: {type: Schema.Types.ObjectId, ref: 'Category'},
-  price: { type: Number, required: true }
-}, {
-  timestamps: true
-});
+const reviewSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 5,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const itemSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    vendor: { type: Schema.Types.ObjectId, ref: "Vendor" },
+    category: { type: Schema.Types.ObjectId, ref: "Category" },
+    price: { type: Number, required: true },
+    reviews: [reviewSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = itemSchema;
