d8771dde663fde0b3f5865f49ffbc2d1175374ef
Alyssa Alonso <aalonso310@gmail.com>
2023-12-18 17:59:47 -0500

    update MarketCard component

--- src/components/MarketCard/MarketCard.jsx
+++ src/components/MarketCard/MarketCard.jsx
@@ -3,6 +3,30 @@ import React from "react";
 export default function MarketCard({ market }) {
   const randomNumber = Math.floor(Math.random() * 1000); // Generating a random number
 
+  const startDateString = market.startDate;
+  const startDate = new Date(startDateString);
+  const startTime = startDate.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+  });
+  const formattedStartDate = startDate.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  const endDateString = market.endDate;
+  const endDate = new Date(endDateString);
+  const endTime = endDate.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+  });
+  const formattedEndDate = endDate.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
   return (
     <div class="market-card">
       <div
@@ -17,9 +41,13 @@ export default function MarketCard({ market }) {
       <div class="card-content">
         <h2>{market.name}</h2>
         <p>{market.address}</p>
-        <p>{market.dayOfWeek}</p>
-        <p>{market.startDate}</p>
-        <p>{market.endDate}</p>
+        <p>
+          {market.dayOfWeek}, {startTime} - {endTime}
+        </p>
+
+        <p>
+          {formattedStartDate} - {formattedEndDate}
+        </p>
 
         <div class="card-links">
           <a href={`/markets/${market.name}`}>View Offerings</a>
