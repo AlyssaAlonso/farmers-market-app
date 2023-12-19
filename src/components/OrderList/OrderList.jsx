f7376f44a35d28d4b0b865b411cb2243b96fb84d
Alyssa Alonso <aalonso310@gmail.com>
2023-12-19 15:43:57 -0500

    remove stylesheet imports from OrderList and OrderListItem components

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -1,17 +1,17 @@
-import './OrderList.css';
-import OrderListItem from '../OrderListItem/OrderListItem';
+import OrderListItem from "../OrderListItem/OrderListItem";
 
 export default function OrderList({ orders, activeOrder, setActiveOrder }) {
-
-  const orderList = orders.map(order => <OrderListItem
-    order={order}
-    activeOrder={activeOrder}
-    setActiveOrder={setActiveOrder}
-    key={order.id}
-  />);
+  const orderList = orders.map((order) => (
+    <OrderListItem
+      order={order}
+      activeOrder={activeOrder}
+      setActiveOrder={setActiveOrder}
+      key={order.id}
+    />
+  ));
 
   return (
-    <main className={`OrderList ${orders.length ? '' : 'no-orders'}`}>
+    <main className={`OrderList ${orders.length ? "" : "no-orders"}`}>
       {orderList}
     </main>
   );
