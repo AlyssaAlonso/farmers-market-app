d9de673107cc0493c8e680552ece34b6f08d784d
Alyssa Alonso <aalonso310@gmail.com>
2023-12-19 23:51:30 -0500

    add redirect route

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -22,7 +22,7 @@ export default function NavBar({ user, setUser }) {
           <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "230px" }}>
             <Nav.Link href="/markets">Markets</Nav.Link>
             <Nav.Link href="/vendors">Vendors</Nav.Link>
-            <Nav.Link href="/items">Items</Nav.Link>
+            <Nav.Link href="/items">All Items</Nav.Link>
             <Nav.Link href="/orders">Orders</Nav.Link>
             <Nav.Link href="/orders/new">Cart</Nav.Link>
             <Nav.Link href="" onClick={handleLogOut}>
